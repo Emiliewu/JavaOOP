bcdd29a70e45d16136ec184c7246bea552305b80
Emilie <emilieweiwu@gmail.com>
2021-02-11 11:53:19 -0800

    update getter and setters

--- pokemon/pokemon/PokemonInterface.java
+++
@@ -1,7 +0,0 @@
-package com.codingdojo.pokemon;
-
-public interface PokemonInterface {
-	Pokemon createPokemon(String name, int health, String type);
-	String pokemonInfo(Pokemon pokemon);
-	void listPokemon();
-}
